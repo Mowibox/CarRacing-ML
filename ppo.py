2f38438e448c58c4de1e8d1a582f2adf098b5eba
Mowibox <ousmane.thiongane@ensea.fr>
2024-12-11 01:27:27 +0100

    Adding 1st part of PPO

--- ppo.py
+++ ppo.py
@@ -3,6 +3,23 @@ import numpy as np
 import torch.nn as nn
 import gymnasium as gym
 import torch.nn.functional as F
+from torch.distributions import Normal
+
+class Gaussian(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, mean_actions, std_actions, old_actions):
+        distribution = Normal(mean_actions, std_actions)
+        actions_with_exploration = distribution.sample()
+
+        if old_actions is None:
+            log_actions = distribution.log_prob(actions_with_exploration)
+        else:
+            log_actions = distribution.log_prob(old_actions)
+
+        return distribution.mean, actions_with_exploration, log_actions, distribution.entropy()
+
 
 class Policy(nn.Module):
     continuous = True
